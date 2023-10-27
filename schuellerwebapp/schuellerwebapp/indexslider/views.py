535e2b73117f0bace4601b2e6b18ddeca36aafb0
hermann wenninger <info@wennhmann.de>
2023-10-27 17:56:53 +0200

    indexslider in view contr. model and template fin

--- schuellerwebapp/schuellerwebapp/indexslider/views.py
+++ schuellerwebapp/schuellerwebapp/indexslider/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Index
 
 # Create your views here.
-def indexslider():
-    pass
+def indexslider(request):
+    indexslider = Index.objects.all()
+    return render(request,'indexslider/index.html',{'indexslider':indexslider})
