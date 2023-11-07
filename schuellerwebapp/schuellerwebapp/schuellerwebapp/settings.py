9755d0313ac89845e792b94af1271905709c14fe
hermann wenninger <info@wennhmann.de>
2023-11-07 18:39:13 +0100

    extreme changes staticfiles

--- schuellerwebapp/schuellerwebapp/schuellerwebapp/settings.py
+++ schuellerwebapp/schuellerwebapp/schuellerwebapp/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "schuellerwebapp.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -120,7 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'schuellerwebapp/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
