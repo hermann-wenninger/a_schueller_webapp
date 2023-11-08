cfff9f74e6069705fc5496a9eb7517cfd7d16ad8
hermann wenninger <info@wennhmann.de>
2023-11-08 14:30:25 +0100

    many jobs around replacement

--- schuellerwebapp/schuellerwebapp/atelier/views.py
+++ schuellerwebapp/schuellerwebapp/atelier/views.py
@@ -5,8 +5,6 @@ from .models import EinleitungAtelier, BilderAtelier
 def atelier(request):
     
     bilder = BilderAtelier.objects.all()
-    return render(request,'atelier/atelierde.html',{'bilder':bilder})
+    return render(request,'atelier/atelier.html',{'bilder':bilder})
 
-def indexslider(request):
-    indexslider = Index.objects.all()
-    return render(request,'indexslider/index.html',{'indexslider':indexslider})
+
